a1b2b435fc6723f6b71d0f9c214b821c7dc0fbe4
Andrew Nixon <andrewnixon0@gmail.com>
2017-10-22 23:27:20 +0100

    Used the Tracker.autorun

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -5,10 +5,6 @@ import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
 
-Tracker.autorun(function() {
-  console.log('Players list', Players.find().fetch());
-});
-
 const renderPlayers = function (playersList) {
   return playersList.map(function (player) {
     return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
@@ -16,17 +12,22 @@ const renderPlayers = function (playersList) {
 };
 
 Meteor.startup(function (){
-  let title = 'Score Keep';
-  let name = 'Andrew';
-  let jsx = (
-    <div>
-      {/* Render h1 tag with title var as text */}
-      <h1>{title}</h1>
-      <p>Hello {name}!</p>
-      <p>This is a second paragraph.</p>
-      {renderPlayers(players)}
-    </div>
-  );
+  Tracker.autorun(function() {
+    console.log('Players list', Players.find().fetch());
+    var players = Players.find().fetch();
 
-  ReactDOM.render(jsx, document.getElementById('app'));
+    let title = 'Score Keep';
+    let name = 'Andrew';
+    let jsx = (
+      <div>
+        {/* Render h1 tag with title var as text */}
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>This is a second paragraph.</p>
+        {renderPlayers(players)}
+      </div>
+    );
+
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
 });
